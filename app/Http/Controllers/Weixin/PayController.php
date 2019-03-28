67164735e98579c2c6fd908da20c2b108f569b56
nihaoa134 <2337865364@qq.com>
2019-03-28 03:28:11 +0000

    1

--- app/Http/Controllers/Weixin/PayController.php
+++ app/Http/Controllers/Weixin/PayController.php
@@ -17,7 +17,7 @@ class PayController extends Controller
         $key = '7c4a8d09ca3762af61e59520943AB26Q';
         $url = "https://api.mch.weixin.qq.com/pay/unifiedorder";
         $ip = $_SERVER['REMOTE_ADDR'];
-        $notify_url = "http://node.lixiaonitongxue.top/wxstatus";
+        $notify_url = "http://ppp.lixiaonitongxue.top/wxstatus";
         $info = array(
             'appid' =>'wxd5af665b240b75d4',
             'mch_id' =>'1500086022',
